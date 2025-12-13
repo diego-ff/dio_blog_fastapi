b995fef980788991f8e3d243c115df786ab6e630
diegoff <diegoff_@gmail.com>
2025-12-13 19:27:14 -0300

    fix: adjust imports to run with uvicorn --app-dir

--- dio_blog/src/database.py
+++ dio_blog/src/database.py
@@ -1,9 +1,9 @@
 import sqlalchemy as sa
 import databases
+import os
 
 
-
-DATABASE_URL = "sqlite:///./blog.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
 database = databases.Database(DATABASE_URL)
 metadata = sa.MetaData()
 engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
