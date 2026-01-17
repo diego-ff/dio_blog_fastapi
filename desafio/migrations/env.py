a301c87e72f0b85f232fff7a4e87b7fa061ad20c
diegoff <diegoff_@gmail.com>
2026-01-17 16:03:19 -0300

    feat: adiciona projeto FastAPI desafio

---
+++ desafio/migrations/env.py
@@ -0,0 +1,61 @@
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+from src.config import settings
+from src.database import metadata  # apenas metadata, NÃO engine
+
+# importa os models para o Alembic enxergar as tabelas
+from src.models.transaction import transactions  # noqa
+from src.models.account import accounts  # noqa
+
+# this is the Alembic Config object
+config = context.config
+
+# seta a URL do banco no alembic dinamicamente
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("+aiosqlite", ""))
+
+# configuração de logs
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = metadata
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = settings.database_url.replace("+aiosqlite", "")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
